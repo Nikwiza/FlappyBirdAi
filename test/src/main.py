91316c1a2a6358a452b1206d192caa41c1dc7628
nikola kerleta <nikola.kerleta1@gmail.com>
2023-05-27 13:19:53 +0200

    Adding scoring and starting reset functionality

--- test/src/main.py
+++ test/src/main.py
@@ -15,6 +15,7 @@ class Game:
         # Sprite groups
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
+        self.agents = pygame.sprite.Group()
 
         # Sprite init
         BG(self.all_sprites)
@@ -23,7 +24,22 @@ class Game:
 
         # Timer
         self.obstacle_timer = pygame.USEREVENT + 1
-        pygame.time.set_timer(self.obstacle_timer, 1400)
+        pygame.time.set_timer(self.obstacle_timer, PIPE_SPAWN_SPEED)
+
+        # Font 
+        self.font = pygame.font.Font(None, SCORE_SIZE)
+        self.score = 0
+
+
+    def restart(self):
+        return 1 #TODO
+
+    def display_score(self):
+        self.score = pygame.time.get_ticks()//1000
+
+        score_surf = self.font.render(str(self.score), True, 'black')
+        score_rect = score_surf.get_rect(topleft = (0, 0))
+        self.display_surface.blit(score_surf, score_rect)
 
     def collisions(self):
         if pygame.sprite.spritecollide(self.bird, self.collision_sprites, False, pygame.sprite.collide_mask)\
@@ -57,12 +73,13 @@ class Game:
 
             # Game logic
 
-
-
             self.all_sprites.update(dt)
             self.collisions()
             self.display_surface.fill("black")
             self.all_sprites.draw(self.display_surface)
+            self.agents.draw(self.display_surface)
+            self.display_score()
+                
 
             pygame.display.update()
             self.clock.tick(FPS)
